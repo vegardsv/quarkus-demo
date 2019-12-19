403233831d99c56ab58d396edd7b8637bf9fbaf5
Vegard Svendsen <vegardsvendsen@Vegards-MBP.kantega.lan>
2019-12-19 15:34:23 +0100

    masse nytt

---
+++ src/main/java/no/java/trd/quarkus/ClockResource.java
@@ -0,0 +1,21 @@
+package no.java.trd.quarkus;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+@Path("/clock")
+public class ClockResource {
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String clock(){
+        SimpleDateFormat formatter= new SimpleDateFormat("yyyy-MM-dd 'at' HH:mm:ss z");
+        Date date = new Date(System.currentTimeMillis());
+        return formatter.format(date);
+    }
+
+}
